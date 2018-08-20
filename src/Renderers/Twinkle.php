b2177042a296c131cc592f028ef011432a901f26
Laraning <bruno.falcao@laraning.com>
2018-08-20 23:18:17 +0200

    File changes

--- src/Renderers/Twinkle.php
+++ src/Renderers/Twinkle.php
@@ -62,36 +62,38 @@ class Twinkle extends Renderer
             $action = $this->action;
         } elseif (@method_exists($namespace, 'default')) {
             $action = 'default';
-        }
+        };
 
-        if (!is_null($action)) {
-            // Obtain route parameters.
-            $router = app()->make('router');
-            $arguments = $router->getCurrentRoute()->parameters;
+        if (is_null($action)) {
+            return;
+        };
 
-            // In case the action is update or store, we need to pass the request.
-            if ($action == 'update' || $action == 'store') {
-                $arguments['request'] = request();
-            }
+        // Obtain route parameters.
+        $router = app()->make('router');
+        $arguments = $router->getCurrentRoute()->parameters;
 
-            // Verify if there are extra parameters in the method that need depedency injection.
-            $ref = new \ReflectionMethod($namespace, $action);
+        // In case the action is update or store, we need to pass the request.
+        if ($action == 'update' || $action == 'store') {
+            $arguments['request'] = request();
+        };
 
-            $extraArguments = [];
-            if (count($ref->getParameters()) > 0) {
-                foreach ($ref->getParameters() as $data) {
-                    $extraArguments[$data->getName()] = app()->make((string) $data->getType());
-                }
-            }
+        // Verify if there are extra parameters in the method that need depedency injection.
+        $ref = new \ReflectionMethod($namespace, $action);
 
-            // Obtain response.
-            $response = app()->call("{$namespace}@{$action}", array_merge($arguments, $extraArguments));
+        $extraArguments = [];
+        if (count($ref->getParameters()) > 0) {
+            foreach ($ref->getParameters() as $data) {
+                $extraArguments[$data->getName()] = app()->make((string) $data->getType());
+            };
+        };
 
-            // Compute response to be arrayable.
-            $response = !is_array($response) ? [$response] : $response;
+        // Obtain response.
+        $response = app()->call("{$namespace}@{$action}", array_merge($arguments, $extraArguments));
 
-            // Merge response with the current data.
-            $this->data = array_merge_recursive((array) $this->data, $response);
-        }
+        // Compute response to be arrayable.
+        $response = !is_array($response) ? [$response] : $response;
+
+        // Merge response with the current data.
+        $this->data = array_merge_recursive((array) $this->data, $response);
     }
 }
